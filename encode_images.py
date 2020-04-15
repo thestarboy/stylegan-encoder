43c0b4792374ea4e0eba15fb2654e5e28a3e82ca
thestarboy <34975821+thestarboy@users.noreply.github.com>
2020-04-15 11:09:57 +0530

    update

--- encode_images.py
+++ encode_images.py
@@ -121,7 +121,7 @@ def main():
 
     perc_model = None
     if (args.use_lpips_loss > 0.00000001):
-        with dnnlib.util.open_url('https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2', cache_dir=config.cache_dir) as f:
+        with dnnlib.util.open_url('https://drive.google.com/open?id=1bEdrqPQs1ooBOyiFBaii_6DXYy4v_-aX', cache_dir=config.cache_dir) as f:
             perc_model =  pickle.load(f)
     perceptual_model = PerceptualModel(args, perc_model=perc_model, batch_size=args.batch_size)
     perceptual_model.build_perceptual_model(generator, discriminator_network)
